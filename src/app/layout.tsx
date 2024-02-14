d09251eebc29ab591868103054c46cd51b17ee51
HASURO <rentaro170928@icloud.com>
2024-02-15 07:45:44 +0900

    fix: session

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.scss';
-import Footer from '@/components/footer/Footer';
 import NextAuthProvider from '@/providers/NextAuth';
-import { getServerSession } from 'next-auth';
-import LoginBtn from '@/components/ui/button/login/LoginBtn';
-import { authOptions } from './api/auth/[...nextauth]/route';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,13 +15,10 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
   return (
     <html lang='ja'>
       <body className={inter.className}>
-        <LoginBtn session={session} />
         <NextAuthProvider>{children}</NextAuthProvider>
-        <Footer />
       </body>
     </html>
   );
