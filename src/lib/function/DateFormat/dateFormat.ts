5b8e8bbac296398d2c88052b9eae2b7ee4e6b189
Nozomi Fukawa <noppi5959@gmail.com>
2024-02-16 06:06:59 +0900

    add: kusa-function #16

--- src/lib/function/DateFormat/dateFormat.ts
+++ src/lib/function/DateFormat/dateFormat.ts
@@ -1,17 +1,12 @@
 // YYYY-MM-DD to String
 
-export function getCurrentDate(): string {
-  const currentDate: Date = new Date();
-  const year: number = currentDate.getFullYear();
-  const month: number = currentDate.getMonth() + 1;
-  const day: number = currentDate.getDate();
+export function dateFormat(date: Date): string {
+  const year: number = date.getFullYear();
+  const month: number = date.getMonth() + 1;
+  const day: number = date.getDate();
 
   const monthString: string = month < 10 ? `0${month}` : `${month}`;
   const dayString: string = day < 10 ? `0${day}` : `${day}`;
 
   return `${year}-${monthString}-${dayString}`;
 }
-
-// 現在の日付を取得し、指定された形式で出力する
-// const formattedDate: string = getCurrentDate();
-// console.log(formattedDate);
---
+++ src/lib/function/Kusa/getKusaFunction.ts
@@ -0,0 +1,23 @@
+import { dateFormat } from '../DateFormat/dateFormat';
+
+export const getKusa = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}`, {
+      method: 'GET',
+    });
+    const diaries = await res.json();
+    const thisYear = new Date().getFullYear();
+    const resultArray = [];
+    const publishedAtArray = JSON.parse(diaries.diaries.publishedAt);
+    for (let i = 0; i < publishedAtArray.length; i += 1) {
+      const year = publishedAtArray[i].getFullYear();
+      if (thisYear === year) {
+        const publishedAtTodate = dateFormat(publishedAtArray[i]);
+        resultArray.push(publishedAtTodate);
+      }
+    }
+    return resultArray;
+  } catch (error) {
+    return console.log(error);
+  }
+};
