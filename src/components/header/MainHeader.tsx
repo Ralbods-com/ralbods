e552df3a56098853ca85754edc0a2f2540ae80be
HASURO <rentaro170928@icloud.com>
2024-02-18 02:42:17 +0900

    fix: props

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -1,14 +1,13 @@
 'use client';
 
-import { Session } from 'next-auth';
 import CreateDiaryButton from '../ui/button/header/CreateDiaryButton';
 import styles from './header.module.scss';
-import LoginBtn from '../ui/button/login/LoginBtn';
+import LoginButton from '../ui/button/login/LoginButton';
 
-export default function MainHeader({ session }: { session: Session | null }) {
+export default function MainHeader() {
   return (
     <>
-      <LoginBtn session={session} />
+      <LoginButton />
       <div className={styles['container']}>
         <p className={styles['title']}>Ralbods</p>
         <CreateDiaryButton />
