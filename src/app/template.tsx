9506fe734f1363b9b506419d6b8dcd1aaf29c4c2
HASURO <rentaro170928@icloud.com>
2024-02-18 02:44:23 +0900

    add: template

---
+++ src/app/template.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { usePathname, useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+
+export default function RootTemplate({
+  children,
+}: {
+  children: React.ReactNode,
+}) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const { data: session } = useSession();
+
+  if (pathname === '/' && session?.user) {
+    router.push('/ralbods');
+  } else {
+    return children;
+  }
+}
