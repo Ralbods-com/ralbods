94d8b67390f1189fafdf78a1a0c46022e3a28c46
HASURO <rentaro170928@icloud.com>
2024-02-20 19:35:32 +0900

    fix: indent

--- src/types/diaryType.ts
+++ src/types/diaryType.ts
@@ -1,19 +1,18 @@
 import type { TagApiType } from './tagType';
 import type { UserShowType } from './userType';
 
-export type DiaryType =
-  {
-    id: number,
-    date: string,
-    title: string,
-    body: string | null,
-    createdAt: string,
-    publishedAt: string | null,
-    updatedAt: string,
-    userId: string,
-    mind: string,
-    tags: TagApiType[],
-  };
+export type DiaryType = {
+  id: number,
+  date: string,
+  title: string,
+  body: string | null,
+  createdAt: string,
+  publishedAt: string | null,
+  updatedAt: string,
+  userId: string,
+  mind: string,
+  tags: TagApiType[],
+};
 
 export type DiaryShowType = {
   id: number,
