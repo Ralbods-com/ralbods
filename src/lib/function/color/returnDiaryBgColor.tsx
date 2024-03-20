0fcf143fff9be77a552aa75f9dd36f0b1210c30d
Nozomi Fukawa <noppi5959@gmail.com>
2024-03-20 23:06:08 +0900

    hotfix/set-mind-color

--- src/lib/function/color/returnDiaryBgColor.tsx
+++ src/lib/function/color/returnDiaryBgColor.tsx
@@ -1,14 +1,14 @@
-export const returnDiaryBgColor = (mind: string) => {
+export const returnDiaryBgColor = (mind: number) => {
   switch (mind) {
-    case 'maximum':
+    case 4:
       return 'rgba(var(--red), 0.2)';
-    case 'happy':
+    case 3:
       return 'rgba(var(--yellow), 0.2)';
-    case 'normal':
+    case 2:
       return 'rgb(var(--pale-gray))';
-    case 'sad':
+    case 1:
       return 'rgba(var(--main-color), 0.2)';
-    case 'bad':
+    case 0:
       return 'rgba(var(--sub-color), 0.2)';
     default:
       return 'rgb(var(--pale-gray))';
